d0b98f2579f0f995ea8aad93ff149bc9f6456df9
Adebimpe Sadiq <sadiqadebimpem@gmail.com>
2026-06-07 11:53:45 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+# Main Flask App - GDSS 2026 Hackathon
+# AI-Driven Image-to-Item Master Data Tool
+
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True)
